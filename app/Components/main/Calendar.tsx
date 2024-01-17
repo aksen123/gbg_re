87907778e7a4562a5c8f8222a845fb3c257d80c0
mw <minwook0523@gmail.com>
2024-01-17 23:51:16 +0900

    pc달력 간격조정4

--- app/Components/main/Calendar.tsx
+++ app/Components/main/Calendar.tsx
@@ -6,41 +6,66 @@ const Calendar = () => {
   const [year, setYear] = useState(new Date().getFullYear());
   const [month, setMonth] = useState(new Date().getMonth() + 1);
 
+  const changeMonth = (num: number) => {
+    if (month + num > 12) {
+      setYear(year + 1);
+      setMonth(1);
+    } else if (month + num < 1) {
+      setYear(year - 1);
+      setMonth(12);
+    } else {
+      setMonth(month + num);
+    }
+  };
   return (
-    <div className="w-full max-w-[1200px]">
-      <div className="w-full h-40 border border-main-color rounded-full overflow-hidden flex justify-start items-center">
+    <div className="w-full max-w-[1200px] mt-20">
+      <div className="w-full h-40 border border-main-color rounded-full overflow-hidden flex justify-start items-center max-[1024px]:hidden">
         <div className="relative w-1/4 h-full bg-main-color text-center text-white">
           <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-4xl font-semibold">
             이달의 일정
           </span>
         </div>
         <div className="flex justify-between items-center gap-1 px-2">
-          <button>{"<"}</button>
+          <button
+            onClick={() => {
+              changeMonth(-1);
+            }}
+          >
+            {"<"}
+          </button>
           <div>
             <p className="font-semibold text-main-color text-center">{month}</p>
             <p>{year}</p>
           </div>
-          <button>{">"}</button>
+          <button
+            onClick={() => {
+              changeMonth(1);
+            }}
+          >
+            {">"}
+          </button>
         </div>
-        <div className="grow flex justify-between">
-          {changeCalendar(year, month).map((day, i) => (
-            <Link
-              href="#"
-              key={i}
-              className={
-                "font-medium text-small-text " +
-                `${
-                  i % 7 === 0
-                    ? "text-red-600"
-                    : i % 7 === 6
-                    ? "text-blue-600"
-                    : ""
-                }`
-              }
-            >
-              {day}
-            </Link>
-          ))}
+        <div className="flex justify-between gap-3 px-5 grow">
+          {changeCalendar(year, month).map((day, i) =>
+            day !== "" ? (
+              <Link
+                href="#"
+                key={i}
+                className={
+                  "font-medium text-small-text " +
+                  `${
+                    i % 7 === 0
+                      ? "text-red-600"
+                      : i % 7 === 6
+                      ? "text-blue-600"
+                      : ""
+                  }`
+                }
+              >
+                {day}
+              </Link>
+            ) : null
+          )}
         </div>
       </div>
     </div>
@@ -49,7 +74,7 @@ const Calendar = () => {
 
 const changeCalendar = (year: number, month: number) => {
   let month_day = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  if (month == 2 && checkLeapYear(year)) month_day[0] = 29;
+  if (month == 2 && checkLeapYear(year)) month_day[1] = 29;
   let calendar_arr = [];
   let first_day = new Date(`${year}-${month}`).getDay();
   for (let i = 0; i < first_day; i++) {
